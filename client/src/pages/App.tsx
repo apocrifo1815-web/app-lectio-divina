dc57f8972d216bbeef3fe9cce65b583690177dc7
apocrifo1815-web <apocrifo1815@gmail.com>
2026-03-06 14:07:39 -0300

    Create App.tsx

---
+++ client/src/pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import lectioData from "./dados-leitura.json";
+
+function App() {
+  // O resto do seu código que exibe o Evangelho...
+  return (
+    <div className="App">
+      <h1>Lectio Divina</h1>
+      {/* Aqui vai o seu código de exibição */}
+    </div>
+  );
+}
+
+export default App;
